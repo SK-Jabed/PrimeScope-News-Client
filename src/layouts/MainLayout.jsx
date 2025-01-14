3daf849004829cfa44504761c26db5b44049f841
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-14 19:08:07 +0600

    Layouts and Dynamic Title Implementation Completed

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Shared/Navbar/Navbar";
+import Footer from "../components/Shared/Footer/Footer";
+const MainLayout = () => {
+  return (
+    <div className="bg-white">
+      <Navbar />
+      {/* Dynamic Content Starts Here*/}
+      <div className="pt-24 min-h-[calc(100vh-68px)]">
+        <Outlet />
+      </div>
+      {/* Dynamic Content Ends Here*/}
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
