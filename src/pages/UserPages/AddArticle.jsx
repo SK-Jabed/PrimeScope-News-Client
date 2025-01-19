922e3a32b98bff58bd3758573bdc8df3e14de404
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-20 03:13:03 +0600

    Add Articles Page and Form Updated

--- src/pages/UserPages/AddArticle.jsx
+++ src/pages/UserPages/AddArticle.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import AddArticleForm from "../../components/Forms/AddArticleForm";
 
 const AddArticle = () => {
-    return (
-        <div>
-            Add a Article
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Add Article | PrimeScope News</title>
+      </Helmet>
+
+      {/* Form */}
+      <AddArticleForm />
+    </div>
+  );
 };
 
 export default AddArticle;
