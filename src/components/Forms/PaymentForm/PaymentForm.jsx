bcea3c6bea0f944de5cd8deb97a82d4084d9543e
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-25 22:48:06 +0600

    Payment Functionalities with Stripe completed

--- src/components/Forms/PaymentForm/PaymentForm.jsx
+++ src/components/Forms/PaymentForm/PaymentForm.jsx
@@ -5,13 +5,17 @@ import Button from "../../Shared/Buttons/Button";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import "./PaymentForm.css";
+import useAuth from "../../../hooks/useAuth";
 
 const PaymentForm = ({ plan }) => {
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState("");
   const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
+  const navigate = useNavigate();
 
   const subscriptionPrice = plan?.price;
 
@@ -47,11 +51,57 @@ const PaymentForm = ({ plan }) => {
     });
 
     if (error) {
-      console.log("payment error", error);
+      console.log("Payment error", error);
       setError(error.message);
     } else {
-      console.log("payment method", paymentMethod);
+      console.log("Payment successful", paymentMethod);
       setError("");
+    }
+
+    // Confirm Payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log("Confirm error");
+    } else {
+      console.log("Payment intent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        console.log("Transaction id", paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+      }
+
+      // Now Save The Payment Information in The Database
+      const paymentData = {
+        email: user?.email,
+        name: user?.displayName,
+        price: subscriptionPrice,
+        transactionId: paymentIntent.id,
+        date: new Date(), // utc date convert. use moment js to
+      };
+
+      const res = await axiosSecure.post("/payments", paymentData);
+
+      console.log("Payment saved", res.data);
+
+      if (res.data?.paymentResult?.insertedId) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Thank you for the taka paisa",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate("/subscription");
+      }
     }
   };
 
@@ -77,6 +127,9 @@ const PaymentForm = ({ plan }) => {
         <option>{plan.title}</option>
       </select>
       <p className="text-red-600 mb-3">{error}</p>
+      {transactionId && (
+        <p className="text-green-600 mb-3"> Your transaction id: {transactionId}</p>
+      )}
       <Button
         type="submit"
         disabled={!stripe || !clientSecret}
