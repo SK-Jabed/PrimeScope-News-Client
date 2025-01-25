9f4da7669286e2ee97ef40f856c1b243bb4ff475
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-25 22:48:56 +0600

    Payment Functionalities with Stripe completed

--- src/components/Forms/PaymentForm/PaymentForm.jsx
+++ src/components/Forms/PaymentForm/PaymentForm.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import "./PaymentForm.css";
 import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const PaymentForm = ({ plan }) => {
   const stripe = useStripe();
