47c21ff1f7c3df51a1b6375e0f79b9ae7c47cf3a
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-26 23:45:31 +0600

    Dashboards sidebar updated of client side

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -76,11 +76,7 @@ const Sidebar = () => {
         <div>
           <hr />
 
-          <MenuItem
-            icon={FcSettings}
-            label="Profile"
-            address="/profile"
-          />
+          <MenuItem icon={FcSettings} label="Profile" address="/profile" />
           <button
             onClick={logOut}
             className="flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300 hover:text-gray-700 transition-colors duration-300 transform"
